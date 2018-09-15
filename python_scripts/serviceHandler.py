3885922b08945ac35b9957f95b7d3ee1f697ffca
Nick Pham <vinh.pham@evolus.vn>
2018-09-15 13:26:27 +0700

    Fix service handler

--- python_scripts/serviceHandler.py
+++ python_scripts/serviceHandler.py
@@ -21,37 +21,30 @@ def inGameNotice(c):
         messages = sql["ingame_notice.messages"]
         for m in messages.split(messageDelimeter):
             utils.show("Sending \"%s\" to ps_game..." % m)
-            utils.executeCommand(c + m)
+            utils.executeCommand(c % ("/nt " + m))
             time.sleep(15)
 
         interval = int(sql["ingame_notice.interval"])
         utils.show("Waiting for %d second(s) to notice \"%s\"" % (interval, messages))
         time.sleep(interval)
 
-def vchkoff(c):
-    utils.show("Sending /vchkoff to ps_login...")
-    utils.executeCommand(c)
-
-def vchkon(c):
-    utils.show("Sending /vchkon to ps_login...")
-    utils.executeCommand(c)
-
-def nprotectoff(c):
-    utils.show("Sending /nprotectoff to ps_game...")
-    utils.executeCommand(c)
-
 if __name__== "__main__":
     choice = sys.argv[1]
     if choice == "inGameNotice":
-        psGameExec = psGameExec % "/nt "
         cmd = sqlCmd % psGameExec
-        inGameNotice(servuceCmd)
-    else choice == "startService":
+        inGameNotice(cmd)
+    elif choice == "nprotectoff":
         psGameExec = psGameExec % "/nprotectoff"
         cmd = sqlCmd % psGameExec
-        nprotectoff(serviceCmd)
+        utils.executeCommand(cmd)
+    elif choice == "vchkoff":
         psLoginExec = psLoginExec % "/vchkoff"
         cmd = sqlCmd % psLoginExec
-        vchkoff(serviceCmd)
+        utils.executeCommand(cmd)
+    elif choice == "vchkon":
+        psLoginExec = psLoginExec % "/vchkon"
+        cmd = sqlCmd % psLoginExec
+        utils.executeCommand(cmd)
+
 
 

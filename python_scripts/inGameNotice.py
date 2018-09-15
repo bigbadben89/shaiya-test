776d30f8de2ef3dc4c2cad3e52159e20dc5311d7
Nick Pham <vinh.pham@evolus.vn>
2018-09-15 09:56:31 +0700

    Fix ingame notice

--- python_scripts/inGameNotice.py
+++ python_scripts/inGameNotice.py
@@ -5,11 +5,8 @@ import time
 noticeConf = os.path.join("..", "config", "notice.conf")
 sqlCmd = "python sqlHandler.py executeQuery \"%s\""
 noticeExec = "EXEC OMG_GameWEB.dbo.command @serviceName = N'ps_game', @cmmd = N'/nt %s'"
-<<<<<<< HEAD
-messageDelimeter = "|"
-=======
 fullNoticeCmd = sqlCmd % noticeExec
->>>>>>> 0b6c0813729d57dbe3669dd5b1ed35ef81d333b3
+messageDelimeter = "|"
 
 def main():
     utils.show("Starting ingame auto notice...")
@@ -19,21 +16,16 @@ def main():
         if enabled.lower() == "false":
             utils.show("Ingame notice enabled = %s" % enabled)
             continue
-<<<<<<< HEAD
+
         messages = sql["ingame_notice.messages"]
         for m in messages.split(messageDelimeter):
-            utils.show("Sending \"%s\" to ps_game..." % message)
-            utils.executeCommand(sqlCmd % noticeExec)
+            utils.show("Sending \"%s\" to ps_game..." % m)
+            utils.executeCommand(fullNoticeCmd % m)
             time.sleep(15)
-=======
-        
+
         interval = int(sql["ingame_notice.interval"])
-        message = sql["ingame_notice.message"]
-        utils.show("Waiting for %d second(s) to notice \"%s\"" % (interval, message))
+        utils.show("Waiting for %d second(s) to notice \"%s\"" % (interval, messages))
         time.sleep(interval)
-        utils.show("Sending \"%s\" to ps_game..." % message)
-        utils.executeCommand(fullNoticeCmd % message)
->>>>>>> 0b6c0813729d57dbe3669dd5b1ed35ef81d333b3
 
 if __name__== "__main__":
     main()

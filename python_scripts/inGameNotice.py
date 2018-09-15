a240fd77ad646ccb6a15ff08c17d0d99343b51d1
Nick Pham <vinh.pham@evolus.vn>
2018-09-15 09:43:53 +0700

    Fix ingame notice with multiple messages

--- python_scripts/inGameNotice.py
+++ python_scripts/inGameNotice.py
@@ -4,6 +4,7 @@ import os
 noticeConf = os.path.join("..", "config", "notice.conf")
 sqlCmd = "python sqlHandler.py executeQuery %s"
 noticeExec = "EXEC OMG_GameWEB.dbo.command @serviceName = N'ps_game', @cmmd = N'/nt %s'"
+messageDelimeter = "|"
 
 def main():
     while True:
@@ -14,9 +15,11 @@ def main():
         if lower(enabled) == "false":
             utils.show("Ingame notice enabled = %s" % enabled)
             continue
-        message = sql["ingame_notice.message"]
-        utils.show("Sending \"%s\" to ps_game..." % message)
-        utils.executeCommand(sqlCmd % noticeExec)
+        messages = sql["ingame_notice.messages"]
+        for m in messages.split(messageDelimeter):
+            utils.show("Sending \"%s\" to ps_game..." % message)
+            utils.executeCommand(sqlCmd % noticeExec)
+            time.sleep(15)
 
 if __name__== "__main__":
     main()

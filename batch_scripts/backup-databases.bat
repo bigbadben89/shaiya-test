b49ffc9d73621657bd3e92dd0e7bbcc9a5db9c39
Nick Pham <nick4ever@evolus.localdomain>
2018-09-17 16:00:34 +0700

    add timeout to batch scripts

--- batch_scripts/backup-databases.bat
+++ batch_scripts/backup-databases.bat
@@ -1,4 +1,5 @@
 echo "Backing database..."
+timeout /T 10
 python ..\python_scripts\sqlHandler.py backup
 
 exit
--- batch_scripts/clean-logs.bat
+++ batch_scripts/clean-logs.bat
@@ -1,3 +1,4 @@
 echo "Cleaning ActionLog & ChatLog..."
+timeout /T 10
 python ..\python_scripts\sqlHandler.py executeFile ..\sql\truncate_log.sql
 exit
